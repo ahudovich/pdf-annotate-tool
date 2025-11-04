173a489a0090f0c089e9fe43b5a6f73388aec0df
Andrei Hudovich <andrei@hudovich.com>
2025-11-04 17:47:05 +0100

    chore: clean up

--- components/app/viewer/ViewerHighlightContainer.tsx
+++ components/app/viewer/ViewerHighlightContainer.tsx
@@ -1,61 +1,16 @@
 import {
-  AreaHighlight,
   MonitoredHighlightContainer,
   TextHighlight,
   Tip,
   useHighlightContainerContext,
-  usePdfHighlighterContext,
   ViewportHighlight,
 } from 'react-pdf-highlighter-extended'
 import { HighlightItem } from '@/types/db'
 
-interface ViewerHighlightContainerProps {
-  editHighlight: (idToUpdate: string, edit: Partial<HighlightItem>) => void
-  onContextMenu?: (
-    event: React.MouseEvent<HTMLDivElement>,
-    highlight: ViewportHighlight<HighlightItem>
-  ) => void
-}
+export function ViewerHighlightContainer() {
+  const { highlight, isScrolledTo } = useHighlightContainerContext<HighlightItem>()
 
-export function ViewerHighlightContainer({
-  editHighlight,
-  onContextMenu,
-}: ViewerHighlightContainerProps) {
-  const { highlight, viewportToScaled, screenshot, isScrolledTo, highlightBindings } =
-    useHighlightContainerContext<HighlightItem>()
-
-  const { toggleEditInProgress } = usePdfHighlighterContext()
-
-  const component =
-    highlight.type === 'text' ? (
-      <TextHighlight
-        isScrolledTo={isScrolledTo}
-        highlight={highlight}
-        onContextMenu={(event) => onContextMenu && onContextMenu(event, highlight)}
-      />
-    ) : (
-      <AreaHighlight
-        isScrolledTo={isScrolledTo}
-        highlight={highlight}
-        onChange={(boundingRect) => {
-          const edit = {
-            position: {
-              boundingRect: viewportToScaled(boundingRect),
-              rects: [],
-            },
-            content: {
-              image: screenshot(boundingRect),
-            },
-          }
-
-          editHighlight(highlight.id, edit)
-          toggleEditInProgress(false)
-        }}
-        bounds={highlightBindings.textLayer}
-        onContextMenu={(event) => onContextMenu && onContextMenu(event, highlight)}
-        onEditStart={() => toggleEditInProgress(true)}
-      />
-    )
+  const component = <TextHighlight isScrolledTo={isScrolledTo} highlight={highlight} />
 
   const highlightTip: Tip = {
     position: highlight.position,
--- components/app/viewer/viewer.tsx
+++ components/app/viewer/viewer.tsx
@@ -2,25 +2,21 @@
 
 import { useEffect, useEffectEvent, useRef, useState } from 'react'
 import { PdfHighlighter, PdfLoader } from 'react-pdf-highlighter-extended'
-import { ViewerCommentForm } from '@/components/app/viewer/ViewerCommentForm'
 import { ViewerHighlightContainer } from '@/components/app/viewer/ViewerHighlightContainer'
 import { ViewerToolbar } from '@/components/app/viewer/ViewerToolbar'
 import { ViewerTooltip } from '@/components/app/viewer/ViewerTooltip'
 import { useDatabase } from '@/contexts/db.context'
 import { useHighlights } from '@/contexts/highlights.context'
 import { cn } from '@/lib/utils'
-import type { PdfHighlighterUtils, Tip, ViewportHighlight } from 'react-pdf-highlighter-extended'
-import type { ViewerContextMenuProps } from '@/components/app/viewer/ViewerContextMenu'
-import type { HighlightItem } from '@/types/db'
+import type { PdfHighlighterUtils } from 'react-pdf-highlighter-extended'
 
 export function Viewer({ className }: { className?: string }) {
   const highlighterUtilsRef = useRef<PdfHighlighterUtils | null>(null)
 
-  const [contextMenu, setContextMenu] = useState<ViewerContextMenuProps | null>(null)
   const [pdfScaleValue, setPdfScaleValue] = useState<number | undefined>(undefined)
 
   const { documents, selectedDocumentId } = useDatabase()
-  const { highlights, addHighlight, deleteHighlight, editHighlight } = useHighlights()
+  const { highlights, addHighlight } = useHighlights()
 
   const filteredHighlights = highlights.filter(
     (highlight) => highlight.documentId === selectedDocumentId
@@ -56,40 +52,6 @@ export function Viewer({ className }: { className?: string }) {
 
   const fileUrl = URL.createObjectURL(pdfDocument.file)
 
-  function handleContextMenu(
-    event: React.MouseEvent<HTMLDivElement>,
-    highlight: ViewportHighlight<HighlightItem>
-  ) {
-    event.preventDefault()
-
-    setContextMenu({
-      xPos: event.clientX,
-      yPos: event.clientY,
-      deleteHighlight: () => deleteHighlight(highlight.id),
-      editComment: () => editComment(highlight),
-    })
-  }
-
-  function editComment(highlight: ViewportHighlight<HighlightItem>) {
-    if (!highlighterUtilsRef.current) return
-
-    const editCommentTip: Tip = {
-      position: highlight.position,
-      content: (
-        <ViewerCommentForm
-          onSubmit={(input: string) => {
-            editHighlight(highlight.id, { comment: input })
-            highlighterUtilsRef.current!.setTip(null)
-            highlighterUtilsRef.current!.toggleEditInProgress(false)
-          }}
-        />
-      ),
-    }
-
-    highlighterUtilsRef.current.setTip(editCommentTip)
-    highlighterUtilsRef.current.toggleEditInProgress(true)
-  }
-
   return (
     <ViewerWrapper className={cn('relative', className)}>
       <ViewerToolbar name={pdfDocument.name} setPdfScaleValue={setPdfScaleValue} />
@@ -106,10 +68,7 @@ export function Viewer({ className }: { className?: string }) {
               highlighterUtilsRef.current = pdfHighlighterUtils
             }}
           >
-            <ViewerHighlightContainer
-              editHighlight={editHighlight}
-              onContextMenu={handleContextMenu}
-            />
+            <ViewerHighlightContainer />
           </PdfHighlighter>
         )}
       </PdfLoader>
